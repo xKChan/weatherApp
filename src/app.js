92d5a4b7711d2b308f535fb44a59867e121671d7
kc <xkylechan@gmail.com>
2023-11-05 17:28:12 -0700

    Got all information from API and displayed basically on browser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const cityInput = document.getElementById("city");
+const cityName = document.getElementById("cityName");
+const forecast = document.getElementById("forecast");
+const tempNow = document.getElementById("tempNow");
+const feels = document.getElementById("feels");
+const low = document.getElementById("lows");
+const highs = document.getElementById("highs");
+const humidity = document.getElementById("humidity");
 
 const site = "http://api.weatherapi.com/v1";
-const currentWeather = "/current.json";
+const currentWeather = "/forecast.json";
 const key = "e2cdef5400cf4cf5ab9234643230211";
 let city = "calgary";
 
@@ -13,17 +20,13 @@ export function getWeather() {
       return response.json();
     })
     .then(function (response) {
-      console.log(response.current.condition.text);
-      console.log(response.location.name);
-      console.log(response.current.temp_c);
+      console.log(response);
+      forecast.textContent = response.current.condition.text;
+      cityName.textContent = `${response.location.name}, ${response.location.country}`;
+      tempNow.textContent = response.current.temp_c;
+      feels.textContent = `Feels Like: ${response.current.feelslike_c}`;
+      low.textContent = `Low: ${response.forecast.forecastday[0].day.mintemp_c}`;
+      highs.textContent = `High: ${response.forecast.forecastday[0].day.maxtemp_c}`;
+      humidity.textContent = `Humidity: ${response.current.humidity}`;
     });
 }
-
-// function getCity() {
-//   city = cityInput.value;
-//   console.log(city);
-// }
-
-// cityInput.addEventListener("input", () => {
-//   getCity();
-// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
